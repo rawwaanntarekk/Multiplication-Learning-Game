7aed181823adccf9122a7c7dcda7a2a87ef55186
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-09-03 14:18:30 +0300

    Display the home screen with menu options.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
+    static int lowerBound = 1;
+    static int upperBound = 10;
+
+    public static int generateRandomNumber() {
+        return (int) (Math.random() * (upperBound - lowerBound + 1) + lowerBound);
+    }
+
+
+    public static void displayHomeScreen(){
+        System.out.println("Welcome to the Multiplication Learning Game!");
+        System.out.println("1. Start Game\n2. Help\n3. Show High Score\n4. Exit");
+
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
